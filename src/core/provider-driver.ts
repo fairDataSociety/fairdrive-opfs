3c99b1c67bf8029345721cfe806ab3b3c8a24e85
Rogelio Morrell <molekilla@gmail.com>
2023-01-12 13:13:09 -0500

    feat: download / upload

--- src/core/provider-driver.ts
+++ src/core/provider-driver.ts
@@ -4,10 +4,41 @@ import { Mount, Entries } from './provider'
  * ProviderDriver is the interface that all providers must implement that adheres for W3C FileSystem API.
  */
 export interface ProviderDriver {
+  /**
+   * Reuturns true if the file or directory exists
+   * @param name - The name of the file or directory
+   * @param mount - mount point
+   * @returns A boolean value indicating whether the file or directory exists
+   */
   exists: (name: string, mount: Mount) => Promise<boolean>
   createDir: (name: string, mount: Mount) => Promise<any>
+  /**
+   * Delete a file or directory
+   * @param name - name of the file or directory
+   * @param mount - mount point
+   * @returns Returns true if the file or directory was deleted, else false
+   */
   delete: (name: string, mount: Mount) => Promise<any>
+  /**
+   * Read entries from a mount point
+   * @param mount - mount point
+   * @returns Entries
+   */
   read: (mount: Mount) => Promise<Entries>
+  /**
+   * Download entries from a mount point
+   * @param name - name of the file
+   * @param mount - mount point
+   * @param options - options
+   * @returns Result
+   */
   download: (name: string, mount: Mount, options: any) => Promise<any>
+  /**
+   * Upload entries from a file
+   * @param file - file to upload
+   * @param mount - mount point
+   * @param options - options
+   * @returns Result
+   */
   upload: (file: File, mount: Mount, options: any) => Promise<any>
 }
--- test/unit/fairdrive-connector.spec.ts
+++ test/unit/fairdrive-connector.spec.ts
@@ -4,6 +4,13 @@ import { faker } from '@faker-js/faker'
 import { FdpConnectModule } from '../../src/core/module'
 import { FairosProvider } from '../../src/providers/fairos'
 import fetchMock from 'jest-fetch-mock'
+import { FormData } from 'formdata-polyfill/esm.min.js'
+import { Blob } from 'fetch-blob'
+import { File } from 'fetch-blob/file.js'
+
+global.FormData = FormData
+global.Blob = Blob
+global.File = File
 
 describe('fairdrive connector module', () => {
   let module: FdpConnectModule
@@ -119,6 +126,246 @@ describe('fairdrive connector module', () => {
   })
 
   it('should list files', async () => {
+    const fairosConnector = await module.connect<FairosProvider>('fairos', FairosProvider)
+
+    expect(fairosConnector).toBeDefined()
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        address: faker.finance.ethereumAddress(),
+        message: 'mock response ',
+        nameHash: faker.datatype.hexadecimal({ length: 32, prefix: '', case: 'lower' }),
+        publicKey: faker.datatype.hexadecimal({ length: 64, prefix: '', case: 'lower' }),
+      }),
+    )
+
+    await fairosConnector.userLogin(username, password)
+
+    expect(fetchMock).toHaveBeenCalled()
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        pods: ['panama', 'colombia', 'costa_rica'],
+        sharedPods: ['nicaragua'],
+      }),
+    )
+
+    const mounts = await fairosConnector.listMounts()
+    expect(fetchMock).toHaveBeenCalled()
+    expect(mounts.length).toBe(3)
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        files: [
+          {
+            accessTime: faker.datatype.datetime().getTime(),
+            blockSize: '1024',
+            contentType: faker.system.mimeType(),
+            creationTime: faker.datatype.datetime().getTime(),
+            mode: 0,
+            modificationTime: faker.datatype.datetime().getTime(),
+            name: faker.system.fileName(),
+            size: faker.datatype.number(),
+            tag: 0,
+          },
+        ],
+      }),
+    )
+
+    const fs = await fairosConnector.getFSHandler(mounts[0])
+    const entries = await fs.entries()
+    const entry = await entries.next()
+    expect(entry.value[0].tag).toBe(0)
+    const end = await entries.next()
+    expect(end.done).toBe(true)
+  })
+  it('should upload file', async () => {
+    const fairosConnector = await module.connect<FairosProvider>('fairos', FairosProvider)
+
+    expect(fairosConnector).toBeDefined()
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        address: faker.finance.ethereumAddress(),
+        message: 'mock response ',
+        nameHash: faker.datatype.hexadecimal({ length: 32, prefix: '', case: 'lower' }),
+        publicKey: faker.datatype.hexadecimal({ length: 64, prefix: '', case: 'lower' }),
+      }),
+    )
+
+    await fairosConnector.userLogin(username, password)
+
+    expect(fetchMock).toHaveBeenCalled()
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        pods: ['panama', 'colombia', 'costa_rica'],
+        sharedPods: ['nicaragua'],
+      }),
+    )
+
+    const mounts = await fairosConnector.listMounts()
+    expect(fetchMock).toHaveBeenCalled()
+    expect(mounts.length).toBe(3)
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        dirs: [
+          {
+            accessTime: faker.datatype.datetime().getTime(),
+            blockSize: '1024',
+            contentType: faker.system.mimeType(),
+            creationTime: faker.datatype.datetime().getTime(),
+            mode: 0,
+            modificationTime: faker.datatype.datetime().getTime(),
+            name: faker.system.fileName(),
+            size: faker.datatype.number(),
+          },
+        ],
+      }),
+    )
+
+    const driver = module.getConnectedProviders('fairos')
+    await driver.filesystemDriver.upload(new File([], faker.system.fileName()), mounts[0], {})
+  })
+  it('should download file', async () => {
+    const fairosConnector = await module.connect<FairosProvider>('fairos', FairosProvider)
+
+    expect(fairosConnector).toBeDefined()
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        address: faker.finance.ethereumAddress(),
+        message: 'mock response ',
+        nameHash: faker.datatype.hexadecimal({ length: 32, prefix: '', case: 'lower' }),
+        publicKey: faker.datatype.hexadecimal({ length: 64, prefix: '', case: 'lower' }),
+      }),
+    )
+
+    await fairosConnector.userLogin(username, password)
+
+    expect(fetchMock).toHaveBeenCalled()
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        pods: ['panama', 'colombia', 'costa_rica'],
+        sharedPods: ['nicaragua'],
+      }),
+    )
+
+    const mounts = await fairosConnector.listMounts()
+    expect(fetchMock).toHaveBeenCalled()
+    expect(mounts.length).toBe(3)
+
+    fetchMock.mockResponseOnce(JSON.stringify([200, 100, 222, 234, 256, 89]))
+
+    const driver = module.getConnectedProviders('fairos')
+    await driver.filesystemDriver.download('file', mounts[0], {})
+  })
+  xit('should delete file', async () => {
+    const fairosConnector = await module.connect<FairosProvider>('fairos', FairosProvider)
+
+    expect(fairosConnector).toBeDefined()
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        address: faker.finance.ethereumAddress(),
+        message: 'mock response ',
+        nameHash: faker.datatype.hexadecimal({ length: 32, prefix: '', case: 'lower' }),
+        publicKey: faker.datatype.hexadecimal({ length: 64, prefix: '', case: 'lower' }),
+      }),
+    )
+
+    await fairosConnector.userLogin(username, password)
+
+    expect(fetchMock).toHaveBeenCalled()
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        pods: ['panama', 'colombia', 'costa_rica'],
+        sharedPods: ['nicaragua'],
+      }),
+    )
+
+    const mounts = await fairosConnector.listMounts()
+    expect(fetchMock).toHaveBeenCalled()
+    expect(mounts.length).toBe(3)
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        files: [
+          {
+            accessTime: faker.datatype.datetime().getTime(),
+            blockSize: '1024',
+            contentType: faker.system.mimeType(),
+            creationTime: faker.datatype.datetime().getTime(),
+            mode: 0,
+            modificationTime: faker.datatype.datetime().getTime(),
+            name: faker.system.fileName(),
+            size: faker.datatype.number(),
+            tag: 0,
+          },
+        ],
+      }),
+    )
+
+    const fs = await fairosConnector.getFSHandler(mounts[0])
+    const entries = await fs.entries()
+    const entry = await entries.next()
+    expect(entry.value[0].tag).toBe(0)
+    const end = await entries.next()
+    expect(end.done).toBe(true)
+  })
+  xit('should create dir', async () => {
+    const fairosConnector = await module.connect<FairosProvider>('fairos', FairosProvider)
+
+    expect(fairosConnector).toBeDefined()
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        address: faker.finance.ethereumAddress(),
+        message: 'mock response ',
+        nameHash: faker.datatype.hexadecimal({ length: 32, prefix: '', case: 'lower' }),
+        publicKey: faker.datatype.hexadecimal({ length: 64, prefix: '', case: 'lower' }),
+      }),
+    )
+
+    await fairosConnector.userLogin(username, password)
+
+    expect(fetchMock).toHaveBeenCalled()
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        pods: ['panama', 'colombia', 'costa_rica'],
+        sharedPods: ['nicaragua'],
+      }),
+    )
+
+    const mounts = await fairosConnector.listMounts()
+    expect(fetchMock).toHaveBeenCalled()
+    expect(mounts.length).toBe(3)
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        files: [
+          {
+            accessTime: faker.datatype.datetime().getTime(),
+            blockSize: '1024',
+            contentType: faker.system.mimeType(),
+            creationTime: faker.datatype.datetime().getTime(),
+            mode: 0,
+            modificationTime: faker.datatype.datetime().getTime(),
+            name: faker.system.fileName(),
+            size: faker.datatype.number(),
+            tag: 0,
+          },
+        ],
+      }),
+    )
+
+    const fs = await fairosConnector.getFSHandler(mounts[0])
+    const entries = await fs.entries()
+    const entry = await entries.next()
+    expect(entry.value[0].tag).toBe(0)
+    const end = await entries.next()
+    expect(end.done).toBe(true)
+  })
+  xit('should validate if file exists', async () => {
     const fairosConnector = await module.connect<FairosProvider>('fairos', FairosProvider)
 
     expect(fairosConnector).toBeDefined()
